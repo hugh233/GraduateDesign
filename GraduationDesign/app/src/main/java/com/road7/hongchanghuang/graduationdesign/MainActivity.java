6bf06eb79d29c5bf4c847f84f7ff94acc16954eb
hugh <960472204@qq.com>
2016-03-30 00:29:38 +0800

    updateGD
    first

---
+++ GraduationDesign/app/src/main/java/com/road7/hongchanghuang/graduationdesign/MainActivity.java
@@ -0,0 +1,13 @@
+package com.road7.hongchanghuang.graduationdesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
